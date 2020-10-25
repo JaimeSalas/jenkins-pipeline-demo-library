17cd9cc2ce131ee8f74fb8b0ce2ed36e7fcd6964
Jaime Salas Zancada <jaimesalaszancada@Jaimes-MacBook-Pro.local>
2020-10-25 17:22:53 +0100

    added groovy library files

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # jenkins-pipeline-demo-library
 Shared functionality for Jenkins pipelines using Groovy
+
+> Reference: https://github.com/sixeyed/jenkins-pipeline-demo-library
---
+++ test/vars/AuditToolsTest.groovy
@@ -0,0 +1,23 @@
+package vars
+
+import com.lesfurets.jenkins.unit.BaseRegressionTest
+import org.junit.Before
+import org.junit.Test
+
+class AuditToolsTest extends BaseRegressionTest {
+    @Override
+    @Before
+    public void setUp() throws Exception {
+        super.setUp();
+        callStackPath = "test/vars/callstacks/"
+        binding.setVariable('scm', [:])
+    }
+
+    @Test
+    public void call() throws Exception {
+        def script = loadScript('vars/auditTools.groovy')
+        script.call()
+        printCallStack()
+        testNonRegistration("default")
+    }
+}
